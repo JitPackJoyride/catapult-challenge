356a1e0566e31f9b06f1aa9df35aae27f8b6c77e
JitPackJoyride <ajitkrishna99@gmail.com>
2022-10-31 02:56:28 +0000

    feat: fixed route not working error for backend

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 import express, { json } from 'express'
 import { savingsRouter } from './savings/savings.route'
 import { errorHandlerMiddleware } from './middlewares/errorHandler.middleware'
+import { NotFoundError } from './errors/notFound.error'
 
 const app = express()
 
@@ -13,10 +14,11 @@ app.use(json())
 app.use(savingsRouter)
 
 // Catch-all route handler for any requests to an unknown route
-app.get('*', async (req, res, next) => {
-    res.status(404).send('Route not found')
+app.all('*', (req, res, next) => {
+    throw new NotFoundError('Route not found')
 })
 
+// Express will use this middleware to handle any errors that occur
 app.use(errorHandlerMiddleware)
 
 export { app }
